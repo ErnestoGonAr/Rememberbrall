3298cf68b619679a519323db21c564660947e6ee
Edgar23P <edgar_v23@hotmail.com>
2016-05-21 13:48:41 -0500

    adapter

--- app/src/main/java/com/example/ernesto/rememberbrall/Consulta.java
+++ app/src/main/java/com/example/ernesto/rememberbrall/Consulta.java
@@ -1,17 +1,55 @@
 package com.example.ernesto.rememberbrall;
 
-import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+
 
 /**
- * Created by jesusmonroy on 21/05/16.
+ * Created by Edgar on 21/05/16.
  */
-public class Consulta extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.consulta);
+        public class Consulta extends AppCompatActivity {
 
-    }
+            ListView li;
+            ArrayList<BeanPrestamo> lista = new ArrayList<BeanPrestamo>();
+            BeanPrestamo prestamo;
+        public String Convertir(Date FechaD){
+            FechaD=new Date();
+            return FechaD.toString();
+        }
 
-}
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.consulta);
+
+                li=(ListView)findViewById(R.id.Lista);
+                BDHandler baseDedatos = new BDHandler(getApplicationContext());
+                SQLiteDatabase db = baseDedatos.getWritableDatabase();
+
+                Cursor c=db.rawQuery("SELECT * FROM Prestamo;", null);
+                if(c.moveToFirst()){
+                    do{
+                        prestamo=
+                                new BeanPrestamo(
+                                        c.getColumnName(0),c.getColumnName(1),c.getColumnName(2),c.getColumnName(3),c.getColumnName(4));
+                        lista.add(prestamo);
+                    }while(c.moveToNext());
+                }
+                ArrayAdapter<BeanPrestamo> tuano= new ArrayAdapter<BeanPrestamo>(getApplicationContext(),R.layout.item,lista);
+                li.setAdapter(tuano);
+            }
+
+
+
+
+
+        }
