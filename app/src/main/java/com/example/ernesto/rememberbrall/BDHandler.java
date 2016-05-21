f728eaae0dce70bbeaaf1ae3d58dfa28e4b92a31
ErnestoGonAr <ergonzalezar@ittepic.edu.mx>
2016-05-21 10:49:47 -0500

    Modificando BD

--- app/src/main/java/com/example/ernesto/rememberbrall/BDHandler.java
+++ app/src/main/java/com/example/ernesto/rememberbrall/BDHandler.java
@@ -16,6 +16,7 @@ public class BDHandler  extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         //Creacion de las tablas que vamos a utilizar
